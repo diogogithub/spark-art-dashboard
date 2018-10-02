1310b92a3393975fdd52507e0b74000f91e89a13
sesteves <sroesteves@gmail.com>
2018-10-02 21:20:54 +0100

    sleep is not necessary since nc is blocking

--- log-provider.sh
+++ log-provider.sh
@@ -6,9 +6,6 @@ FILENAME=$(ls stats-* | tail -n 1)
 # number of lines to read
 N=100
  
-# time to sleep in milliseconds
-SLEEP=100
- 
 # chunk filename
 CHUNK_FILENAME="log-provider-chunk.csv"
  
@@ -17,5 +14,4 @@ header=$(head -n 1 $FILENAME)
 while true; do
   paste <(echo $header) <(tail -n +2 $FILENAME | tail -n $N) --d '' > $CHUNK_FILENAME
   { echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c $CHUNK_FILENAME)\r\nAccess-Control-Allow-Origin: *\r\n\r"; cat $CHUNK_FILENAME; }  | nc -v -l -p 4870
-  sleep ${SLEEP}e-3
 done
