fe4906d21ad31607b63b0ac158de1208cfbd48a5
Diogo Cordeiro <diogo@fc.up.pt>
2018-10-03 03:48:27 +0100

    Update Ingestion Rate Controller protocol

--- ingestion-rate.js
+++ ingestion-rate.js
@@ -1,4 +1,6 @@
-var current_ingestion_rate = document.getElementById("ingestion-rate").value;
+var current_ingestion_rate_min = document.getElementById("ingestion-rate-min").value;
+var current_ingestion_rate_max = document.getElementById("ingestion-rate-max").value;
+var current_ingestion_rate_per = document.getElementById("ingestion-rate-per").value;
 var ws = new WebSocket('ws://localhost:3030/');
 
 ws.addEventListener('open', function open() {
@@ -15,8 +17,16 @@ ws.addEventListener('message', function message(data, flags) {
 });
 
 document.getElementById("ingestion-rate-update").addEventListener('click', function() {
-  if (document.getElementById("ingestion-rate").value != current_ingestion_rate) {
-    ws.send(document.getElementById("ingestion-rate").value, {mask: true});
-    current_ingestion_rate = "set::" + document.getElementById("ingestion-rate").value;
+  if (document.getElementById("ingestion-rate-min").value != current_ingestion_rate) {
+    ws.send(document.getElementById("ingestion-rate-min").value, {mask: true});
+    current_ingestion_rate_min = "set-min::" + document.getElementById("ingestion-rate-min").value;
+  }
+  if (document.getElementById("ingestion-rate-max").value != current_ingestion_rate) {
+    ws.send(document.getElementById("ingestion-rate-max").value, {mask: true});
+    current_ingestion_rate_max = "set-max::" + document.getElementById("ingestion-rate-max").value;
+  }
+  if (document.getElementById("ingestion-rate-per").value != current_ingestion_rate) {
+    ws.send(document.getElementById("ingestion-rate-per").value, {mask: true});
+    current_ingestion_rate_per = "set-period::" + document.getElementById("ingestion-rate-per").value;
   }
 });
